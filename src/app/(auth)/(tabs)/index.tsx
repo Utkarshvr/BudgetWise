1a50a9e6e709ed62f23b5d6be4797f099e714f49
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2025-11-30 17:20:07 +0530

    Refactor index component to use Redirect for navigation
    - Replaced the previous layout with a Redirect to the transactions tab, streamlining the authentication flow.
    - Removed unnecessary imports and components to simplify the code structure.

--- src/app/(auth)/(tabs)/index.tsx
+++ src/app/(auth)/(tabs)/index.tsx
@@ -1,12 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Redirect } from "expo-router";
 
 export default function index() {
-  return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
+  return <Redirect href="/(auth)/(tabs)/transactions" />;
 }
-
-const styles = StyleSheet.create({})
